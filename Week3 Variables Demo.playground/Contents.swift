410824cfef138fb1d075f9b1128c77f8e0f8dd6d
Starman02 <devingrischow2002@gmail.com>
2022-08-31 14:43:07 -0500

    ordering and fixing swift

---
+++ Week3 Variables Demo.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+let target = 25.0
+
+var monday = 5.5
+
+var tuesday = 0.0
+
+var wednesday = 5.0
+
+var thursday = 0.0
+
+var friday = 0.0
+
+var saturday = 6.0
+
+var sunday = 6.0
+
+var total_workhours = monday + tuesday + wednesday + thursday + friday + saturday + sunday
+
+
+if total_workhours >= target{
+    print("You worked \(total_workhours) this week, Good pay week")
+}
+else{
+    print("you worked \(total_workhours), not as good pay week")
+}
+
+print("money")
